587bbbf8e3856a2983148d1095cf982ad77e249d
Lindsey Wells <github email address>
2019-11-24 21:46:09 +0000

    add sessions to applicationcontroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ require './config/environment'
 class ApplicationController < Sinatra::Base
 
   configure do
+    enable :sessions
+    set :session_secret, "secret"
     set :public_folder, 'public'
     set :views, 'app/views'
   end
@@ -17,6 +19,14 @@ class ApplicationController < Sinatra::Base
 
   get "/login" do
     erb :'users/login'
+  end
+
+  post "/signup" do
+    "you've signed up"
+  end
+
+  post "/login" do
+    "you've logged in"
   end
 
 end
