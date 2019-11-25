41544cd7ad6de7c681cda822e08bd2553ae39d1b
Lindsey Wells <github email address>
2019-11-25 21:05:47 +0000

    add patch outfits route

--- app/controllers/outfit_controller.rb
+++ app/controllers/outfit_controller.rb
@@ -38,7 +38,28 @@ class OutfitController < ApplicationController
         end
 
         post '/outfits/:id/edit' do
+            @user=User.find_by_id(session[:user_id])
+            @outfit=Outfit.find_by_id(params[:id])
+            @items=@outfit.items
             erb :'outfits/edit'
         end
+
+        patch '/outfits/:id' do
+            @outfit=Outfit.find_by_id(params[:id])
+            @user=User.find_by_id(session[:user_id])
+          if !session[:user_id]==@outfit.user_id
+            redirect to '/login'
+          elsif
+            params[:date]=="" || params[:item_ids].empty?
+                redirect to "/outfits/#{@item.id}/edit"
+            else
+            @outfit.date=params[:date]
+            @outfit.item_ids=params[:item_ids]
+            @outfit.save
+        redirect to '/outfits' 
+            end
+          end
+        
+        
 
 end
