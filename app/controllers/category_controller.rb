b54b47a76f36da03cbd9e40bc98ead4b5f65ff9c
Lindsey Wells <github email address>
2019-11-29 11:12:33 +0000

    add category view

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -4,4 +4,22 @@ class CategoryController < ApplicationController
         @categories=Category.all
         erb :"/categories/index"
     end
+
+    get "/category/:id" do
+        if Helpers.is_logged_in?(session)
+            @user=User.find_by_id(session[:user_id])
+            @category=Category.find_by_id(params[:id])
+            @items=[]
+            @user.items.each do |item|
+                if item.category_id.to_s==(params[:id])
+                    @items << item
+                end
+            end
+            @itemssorted=@items.sort_by{ |item| Helpers.cost_per_wear(item)}
+        erb :'categories/show'
+        else
+           redirect to "users/login"
+        end
+    end
+    
 end
